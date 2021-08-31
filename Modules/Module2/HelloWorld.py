54c4ea67ff116e02743b4f8afdbe4870b50d7452
CSI-Pablo-DeJesus <89210573+CSI-Pablo-DeJesus@users.noreply.github.com>
2021-08-31 11:57:21 -0400

    Create logo.py

--- Modules/Module2/HelloWorld.py
+++ Modules/Module2/HelloWorld.py
@@ -9,3 +9,14 @@ studentId = input("What is your student id? ")
 print(f"Hello {name}! ")
 print(f"Your Student id is: {studentId} ")
 print(f"Currently, the time is: {current_time} ")
+
+import os.path as path
+from pathlib import path 
+
+myPath = str(path.join)(Path(__file__).parents[2],"images/logo.py"))
+print(myPath)
+with open(myPath, "r") as file: 
+    exec(file.read())
+def printName():
+print(""" 
+,@@@@%     
