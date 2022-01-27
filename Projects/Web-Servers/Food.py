99feb2901e8c7622211814c4edbe8c5b232c9826
CSI-Pablo-DeJesus <89210573+CSI-Pablo-DeJesus@users.noreply.github.com>
2022-01-27 12:02:25 -0400

    Create Food.py

---
+++ Projects/Web-Servers/Food.py
@@ -0,0 +1,8 @@
+class Food:
+    def __init__(self, id:int, uid:str,dish:str,description:str,ingredient:str,measurement:str):
+        self.id = id, 
+        self.uid = uid
+        self.dish = dish
+        self.description = description
+        self.ingredient = ingredient
+        self.measurement = measurement
