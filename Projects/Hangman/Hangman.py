e67424581be39a16a85630cc32a6717f8eb9a5d2
CSI-Pablo-DeJesus <89210573+CSI-Pablo-DeJesus@users.noreply.github.com>
2022-02-14 12:03:51 -0400

    Hangman Update dia F

--- Projects/Hangman/Hangman.py
+++ Projects/Hangman/Hangman.py
@@ -1,144 +1,144 @@
+import ssl
+import urllib
+import urllib.request
 import json
-import os
-from pathlib import Path
-from Food import Food
+import random
+from RandomFood import RandomFood
+#import everything necesarry for the code to run such as json, random, urllib ect
+
+# This is discouraged but it will avoid certificate validation (prevents error)
+ssl._create_default_https_context = ssl._create_unverified_context
+
+foodsURL = "https://random-data-api.com/api/food/random_food"
+#url which is used to access the random foods
+
+req = urllib.request.Request(foodsURL)
+requestData = json.loads(urllib.request.urlopen(req).read())
+
+food:RandomFood = RandomFood(**requestData)
+
+print (food.ingredient)
 
 
-myPath = Path(__file__).parents[0]
-myFilePath = os.path.join(myPath, 'random_food.json')
-data = open(myFilePath, 'r')
- 
+Steps = ["""
+        |----------------+
+        |                |
+        |
+        |
+        |
+        |
+        |
+        |
+        |
+        |
+        ___________________
+        """,
+        """
+        |----------------+
+        new_func()                |
+        |                O
+        |
+        |
+        |
+        |
+        |
+        |
+        |
+        ___________________
+        """,
+        """
+        |----------------+
+        |                |
+        |                O
+        |                |
+        |
+        |
+        |
+        |
+        |
+        |
+        ___________________
+        """,
+        """
+        |----------------+
+        |                |
+        |                O
+        |               /|
+        |
+        |
+        |
+        |
+        |
+        |
+        ___________________
+        """,
+        """
+        |----------------+
+        |                |
+        |                O
+        |               /|\
+        |                
+        |
+        |
+        |
+        |
+        |
+        ___________________
+        """,
+        """
+        |----------------+
+        |                |
+        |                O
+        |               /|\
+        |                |
+        |
+        |
+        |
+        |
+        |
+        ___________________
+        """,
+        """
+        |----------------+
+        |                |
+        |                O
+        |               /|\
+        |                |
+        |               /
+        |
+        |
+        |
+        |
+        ___________________
+        """,
+        """
+        |----------------+
+        |                |
+        |                O
+        |               /|\
+        |                |
+        |               / \
+        |
+        |
+        |
+        |
+        ___________________
+        """]
 
-data = json.load(data)
-food:Food = Food(**data)
+print(Steps[0])
+
+# Start= input
+print(len(food)*" _ ")
 
 
-print(f"ID: {food.id}")
-print(f"UID: {food.uid}")
-print(f"dish: {food.dish}")
-print(f"description: {food.description}")
-print(f"description: {food.description}")
-print(f"ingredient: {food.ingredient}")
-print(f"measurement: {food.measurement}")
+def get_input():
+    while(True):
+        # ask for input
+        Start= input(f"Name a letter for this food: ")
 
-["""
- |----------------+
- |                |
- |
- |
- |
- |
- |
- |
- |
- |
- ___________________
- """,
- """
- 
- ["""
- |----------------+
- |                |
- |                O
- |
- |
- |
- |
- |
- |
- |
- ___________________
- """,
- """
- 
- ["""
- |----------------+
- |                |
- |                O
- |                |
- |
- |
- |
- |
- |
- |
- ___________________
- """,
- """
- 
- ["""
- |----------------+
- |                |
- |                O
- |               /|
- |
- |
- |
- |
- |
- |
- ___________________
- """,
- """
- 
- ["""
- |----------------+
- |                |
- |                O
- |               /|\
- |                
- |
- |
- |
- |
- |
- ___________________
- """,
- """
- 
- ["""
- |----------------+
- |                |
- |                O
- |               /|\
- |                |
- |
- |
- |
- |
- |
- ___________________
- """,
- """
- 
- ["""
- |----------------+
- |                |
- |                O
- |               /|\
- |                |
- |               /
- |
- |
- |
- |
- ___________________
- """,
- """
- 
- ["""
- |----------------+
- |                |
- |                O
- |               /|\
- |                |
- |               / \
- |
- |
- |
- |
- ___________________
- """,
- """
- 
- """]
+        # Input validation
+        if(len(Start) != 1):
+            print("error 101")
+            continue
+        if():
+         return Start
