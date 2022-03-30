733ae4441ab647deca9059bd082d54a7a0456dd2
CSI-Pablo-DeJesus <89210573+CSI-Pablo-DeJesus@users.noreply.github.com>
2022-03-30 11:12:55 -0400

    Update Snake.py

--- Projects/Snake/Snake.py
+++ Projects/Snake/Snake.py
@@ -3,6 +3,13 @@ pygame.init() #initializes the code
 dis=pygame.display.set_mode((400,300))
 pygame.display.update() #updates the changes made to screen
 pygame.display.set_caption('Snake game by Pablo De Jota') #prints message at start of game in pygame screen
+
+blue=(0,0,255)
+red=(255,0,0)
+ if event.type==pygame.QUIT:
+            game_over=True
+    pygame.draw.rect(dis,blue,[200,150,10,10])
+    pygame.display.update()
 game_over=False #continues game
 while not game_over:
     for event in pygame.event.get(): 
